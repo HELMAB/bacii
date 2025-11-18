c7e76733fa5940775a78ca7db039642871fd24cf
Hel Mab <h-mab@beniten.com>
2025-11-18 23:34:10 +0700

    Update code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
         lang: 'km'
       },
       title: 'Home — Dobpi',
-      titleTemplate: '%s — Dobpi',
+      titleTemplate: '%s',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
